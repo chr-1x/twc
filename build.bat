0aaa0d3d4f7c60870397bae1272a294685d447e0
Chronister <andrewlchronister@gmail.com>
2017-03-29 12:28:53 -0700

    Fix windows build to use (and include) curl dll

--- build.bat
+++ build.bat
@@ -6,17 +6,17 @@ echo  TWC -- Build Batch
 echo ====================
 echo.
 
-set DEFINES= 
-set CCFLAGS= /MT %DEFINES% /Gm- /W4 /wd4201 /wd4100 /wd4189 /wd4505 /wd4706 /wd4996 /wd4127 /wd4244 /wd4200 /wd4204
+set DEFINES=
+set CCFLAGS= %DEFINES% /Gm- /W4 /wd4201 /wd4100 /wd4189 /wd4505 /wd4706 /wd4996 /wd4127 /wd4244 /wd4200 /wd4204
 set LIBS= libcurl.lib 
 
 IF NOT EXIST build mkdir build
 pushd build
 
-cl /nologo /I../include %CCFLAGS% ../code/json.c ../code/codegen.c /link /incremental:no /opt:ref /out:twc_codegen.exe /subsystem:console,5.2
+cl /nologo /Z7 /I../include %CCFLAGS% ../code/json.c ../code/codegen.c /link /incremental:no /opt:ref /out:twc_codegen.exe /subsystem:console,5.2
 call twc_codegen.exe ../code/twitter.c ../api.json ../code/twitter_api.h ../code/twitter_api.c
 
-cl /nologo /c /I../include %CCFLAGS% ../code/twitter.c
+cl /nologo /MT /c /I../include %CCFLAGS% ../code/twitter.c
 
 IF NOT EXIST win64 mkdir win64
 lib /nologo twitter.obj /out:win64/libtwc.lib
